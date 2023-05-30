ccbeb68196a446cf01616fec66fb55300d1667b6
ankit0281 <94033670+ankit0281@users.noreply.github.com>
2023-05-30 11:15:25 +0530

    Delete Signup form directory

--- Signup form/sandbox.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('.sinup-form');
-//const username  = document.querySelector('#username');
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const formData = new FormData(form)
-  console.log(formData.entries())
-});
